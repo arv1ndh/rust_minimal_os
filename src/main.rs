627576790c1f5351f059a32e80791e703d53847e
Arvindh Kumar Chandran <arvindhc95@gmail.com>
2025-01-25 22:35:01 -0800

    Writer now implements fmt::write Added new row support, created a single ref to writer to be used to print lines onto the VGA buffer

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,10 @@ mod vga_buffer;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    vga_buffer::print_something();
+    use core::fmt::Write;
+    vga_buffer::WRITER.lock().write_str("Hello World!!").unwrap();
+    write!(vga_buffer::WRITER.lock(), "Test for other data like numbers: {}, {}", 5, 2.0/3.0);
+    write!(vga_buffer::WRITER.lock(), "This is a test for newline to make sure the rows are pushed up");
     loop {}
 }
 
