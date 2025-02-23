b9c18b0e239686e8a611e7c2237f4ebe48f62b0c
Arvindh Kumar Chandran <arvindhc95@gmail.com>
2025-02-22 22:27:06 -0800

    Test framework for no std projects

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,85 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
 mod vga_buffer;
+mod serial;
+
+#[cfg(test)]
+pub fn test_runner(tests: &[&dyn Testable]) {
+    serial_println!("Running {} tests", tests.len());
+    for test in tests {
+        test.run();
+    }
+    exit_qemu(QemuExitCode::Success);
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failure = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
+}
 
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     use core::fmt::Write;
-    vga_buffer::WRITER.lock().write_str("Hello World!!").unwrap();
-    write!(vga_buffer::WRITER.lock(), "Test for other data like numbers: {}, {}", 5, 2.0/3.0);
-    write!(vga_buffer::WRITER.lock(), "This is a test for newline to make sure the rows are pushed up");
+    println!("Hello World!!!");
+
+    #[cfg(test)]
+    test_main();
+
     loop {}
 }
 
+#[cfg(not(test))]
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> !{
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
+}
+
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    serial_println!("[failed]\n");
+    serial_println!("Error {}\n", info);
+    exit_qemu(QemuExitCode::Failure);
+    loop {}
+}
+
+pub trait Testable {
+    fn run(&self) -> ();
+}
+
+impl<T> Testable for T
+where
+    T: Fn(),
+{
+    fn run(&self) {
+        serial_print!("{}...\t", core::any::type_name::<T>());
+        self();
+        serial_println!("[ok]");
+    }
+
+}
+
+
+#[test_case]
+fn trivial_assertion() {
+    assert_eq!(1, 1);
 }
