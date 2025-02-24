a2300e0df438323487054420e12ec4c78e8988c2
Arvindh Kumar Chandran <arvindhc95@gmail.com>
2025-02-23 17:40:48 -0800

    Added integration test framework

--- tests/basic_boot.rs
+++ tests/basic_boot.rs
@@ -7,37 +7,18 @@
 use core::panic::PanicInfo;
 use rust_minimal_os::println;
 
-
-
-
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    use core::fmt::Write;
-    println!("Hello World!!!");
-
-    #[cfg(test)]
     test_main();
-
     loop {}
 }
 
-#[cfg(not(test))]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
-    loop {}
-}
-
-#[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     rust_minimal_os::test_panic_handler(info)
 }
 
-
 #[test_case]
-fn trivial_assertion() {
-    use core::panic;
-
-    assert_eq!(1, 1);
+fn test_println() {
+    println!("test_println output");
 }
